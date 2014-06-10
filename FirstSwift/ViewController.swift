98ba72eacd2aaa3eb058942fbe849098560a4e63
makiuchi <nouzui2007@gmail.com>
2014-06-10 21:48:12 +0900

    新規

---
+++ FirstSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstSwift
+//
+//  Created by makiuchi on 2014/06/10.
+//  Copyright (c) 2014年 makiuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
